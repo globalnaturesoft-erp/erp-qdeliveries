ef1822cb328c82572daffb4678cf1aa3e8e6a9a0
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-10-28 11:08:15 +0700

    fixes type for price column

--- db/migrate/20171027075324_add_price_to_erp_qdeliveries_delivery_details.rb
+++ db/migrate/20171027075324_add_price_to_erp_qdeliveries_delivery_details.rb
@@ -1,5 +1,5 @@
 class AddPriceToErpQdeliveriesDeliveryDetails < ActiveRecord::Migration[5.1]
   def change
-    add_column :erp_qdeliveries_delivery_details, :price, :dicimal
+    add_column :erp_qdeliveries_delivery_details, :price, :decimal
   end
 end

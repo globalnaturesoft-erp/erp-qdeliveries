b3a859a5c5b7ceda3d67821b47f02dea49dbcfb6
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-11-01 23:22:40 +0700

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,13 +28,17 @@ Erp::Qdeliveries::Delivery.all.destroy_all
     status: status[rand(status.count)],
     creator_id: users.order("RANDOM()").first.id
   )
+  puts '==== Q-delivery ' +num.ordinalize+ ' start ('+delivery.code+') ===='
   wareshouse = Erp::Warehouses::Warehouse.where(id: Erp::Warehouses::Warehouse.pluck(:id).sample(2))
   Erp::Orders::OrderDetail.where(id: Erp::Orders::OrderDetail.pluck(:id).sample(rand(1..20))).each do |order_detail|
     delivery_detail = Erp::Qdeliveries::DeliveryDetail.create(
       order_detail_id: order_detail.id,
       delivery_id: delivery.id,
       quantity: rand(1..order_detail.quantity),
+      price: delivery.delivery_type == Erp::Qdeliveries::Delivery::TYPE_CUSTOMER_IMPORT ? rand(0..(order_detail.price/1000))*1000 : nil,
       warehouse_id: wareshouse.second.id
     )
+    puts '====>>>>>> Delivery detail ' +num.ordinalize+ ' complete ('+delivery.code+') ===='
   end
+  puts '==== Complete ===='
 end

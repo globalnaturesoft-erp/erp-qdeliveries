37f3ea665d71af479acc4ebc3e7702f26de373b4
Luan Pham <luanpm88@gmail.com>
2018-03-03 11:50:46 +0700

    update

--- app/models/erp/qdeliveries/delivery.rb
+++ app/models/erp/qdeliveries/delivery.rb
@@ -388,6 +388,7 @@ module Erp::Qdeliveries
 		def import(file)
       spreadsheet = Roo::Spreadsheet.open(file.path)
       header = spreadsheet.row(1)
+
       (2..spreadsheet.last_row).each do |i|
         row = Hash[[header, spreadsheet.row(i)].transpose]
 
